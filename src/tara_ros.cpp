e96dcf51d02ffd76cf9b2d88c029e0f5df32717f
akhileshmalviya <akhileshkumar.m@e-consystems.com>
2017-03-08 17:59:43 +0530

    /stereo/get_IMU output standard sensor_msgs/Imu message

--- src/tara_ros.cpp
+++ src/tara_ros.cpp
@@ -153,7 +153,8 @@ namespace uvc_camera {
 				exposure_sub = node.subscribe ("set_exposure", 1, &taraCamera::callBackExposure, this);
 				brightness_sub = node.subscribe ("set_brightness", 1, &taraCamera::callBackBrightness, this);
 
-				IMU_pub = node.advertise<geometry_msgs::Point>("get_IMU", 1, true);
+				IMU_pub = node.advertise<sensor_msgs::Imu>("get_IMU", 1, true);
+				IMU_inclination_pub = node.advertise<geometry_msgs::Point>("get_inclination", 1, true);
 				IMU_thread = boost::thread(boost::bind(&taraCamera::IMU_enable, this));
 			}
 		}
@@ -603,6 +604,8 @@ namespace uvc_camera {
 		double wGyro = 10.0;
 		double norm;
 
+		getOrientation(g_x, g_y, g_z, a_x, a_y, a_z);
+		
 		// Normalise the accelerometer measurement
 		norm = sqrt(a_x * a_x + a_y * a_y + a_z * a_z);
 		a_x /= norm;
@@ -679,7 +682,7 @@ namespace uvc_camera {
 		IMUValue.x = angleX;
 		IMUValue.y = angleY;
 		IMUValue.z = angleZ;
-		IMU_pub.publish(IMUValue);
+		IMU_inclination_pub.publish(IMUValue);
 
 	}
 	double taraCamera::GetIMUIntervalTime(IMUCONFIG_TypeDef	lIMUConfig)
@@ -904,5 +907,116 @@ namespace uvc_camera {
 				}
 			}
 		}
+	}
+	void taraCamera::getOrientation(double gx, double gy, double gz, double ax, double ay, double az)
+	{
+		float recipNorm;
+		float s0, s1, s2, s3;
+		float qDot1, qDot2, qDot3, qDot4;
+		float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2 ,_8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
+		
+		
+		sensor_msgs::Imu IMUValue;
+		
+		// To convert acceleration from 'mg' to 'm/s2'
+		IMUValue.linear_acceleration.x = ax * 9.80665 / 1000;
+		IMUValue.linear_acceleration.y = ay * 9.80665 / 1000;
+		IMUValue.linear_acceleration.z = az * 9.80665 / 1000;
+		
+		
+		// To convert acceleration from 'dps' to 'radians/second'
+		
+		gx *= 0.0174533;
+		gy *= 0.0174533;
+		gz *= 0.0174533;
+		
+		IMUValue.angular_velocity.x = gx;
+		IMUValue.angular_velocity.y = gy;
+		IMUValue.angular_velocity.z = gz;
+		
+		
+
+		// Rate of change of quaternion from gyroscope
+		qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
+		qDot2 = 0.5f * (q0 * gx + q2 * gz - q3 * gy);
+		qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
+		qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
+
+		// Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+		if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
+
+			// Normalise accelerometer measurement
+			recipNorm = invSqrt(ax * ax + ay * ay + az * az);
+			ax *= recipNorm;
+			ay *= recipNorm;
+			az *= recipNorm;   
+
+			// Auxiliary variables to avoid repeated arithmetic
+			_2q0 = 2.0f * q0;
+			_2q1 = 2.0f * q1;
+			_2q2 = 2.0f * q2;
+			_2q3 = 2.0f * q3;
+			_4q0 = 4.0f * q0;
+			_4q1 = 4.0f * q1;
+			_4q2 = 4.0f * q2;
+			_8q1 = 8.0f * q1;
+			_8q2 = 8.0f * q2;
+			q0q0 = q0 * q0;
+			q1q1 = q1 * q1;
+			q2q2 = q2 * q2;
+			q3q3 = q3 * q3;
+
+			// Gradient decent algorithm corrective step
+			s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
+			s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
+			s2 = 4.0f * q0q0 * q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
+			s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
+			recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
+			s0 *= recipNorm;
+			s1 *= recipNorm;
+			s2 *= recipNorm;
+			s3 *= recipNorm;
+
+			// Apply feedback step
+			qDot1 -= beta * s0;
+			qDot2 -= beta * s1;
+			qDot3 -= beta * s2;
+			qDot4 -= beta * s3;
+		}
+
+		// Integrate rate of change of quaternion to yield quaternion
+		q0 += qDot1 * (1.0f / sampleFreq);
+		q1 += qDot2 * (1.0f / sampleFreq);
+		q2 += qDot3 * (1.0f / sampleFreq);
+		q3 += qDot4 * (1.0f / sampleFreq);
+
+		// Normalise quaternion
+		recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+		q0 *= recipNorm;
+		q1 *= recipNorm;
+		q2 *= recipNorm;
+		q3 *= recipNorm;
+		
+		IMUValue.orientation.w = q0;
+		IMUValue.orientation.x = q1;
+		IMUValue.orientation.y = q2;
+		IMUValue.orientation.z = q3;
+		
+		IMU_pub.publish(IMUValue);		
+	}
+
+	//---------------------------------------------------------------------------------------------------
+	// Fast inverse square-root
+	// See: http://en.wikipedia.org/wiki/Fast_inverse_square_root
+
+	float taraCamera::invSqrt(float x)
+	{
+		float halfx = 0.5f * x;
+		float y = x;
+		long i = *(long*)&y;
+		i = 0x5f3759df - (i>>1);
+		y = *(float*)&i;
+		y = y * (1.5f - (halfx * y * y));
+		return y;
 	}
 };
