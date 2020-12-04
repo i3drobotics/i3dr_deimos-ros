dc98656338ca8be9c42ac01730295d0ebda06bcd
Ben <benknight135@gmail.com>
2020-12-04 11:26:09 +0000

    copy udev rules to lib folder

--- udev/install.sh
+++ udev/install.sh
@@ -5,7 +5,9 @@ INIT_DIR=$PWD
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${SCRIPT_DIR}
 
+# Copy uvc rules to udev
 sudo cp 99-uvc.rules /etc/udev/rules.d/
+sudo cp 99-uvc.rules /lib/udev/rules.d/
 
 # Restore inital working directory
 cd ${INIT_DIR}
